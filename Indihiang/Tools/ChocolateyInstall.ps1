c7cd5c928ab0deccf42eeb2a0f8d3d6fea9c8c30
Jivko Kolev <jivkokgit@gmail.com>
2012-10-08 19:19:16 -0700

    Adding Indihiang

---
+++ Indihiang/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,3 @@
+$package = 'indihiang';
+
+Install-ChocolateyPackage $package 'msi' '/passive' 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=indihiang&DownloadId=112613&FileTime=129139805749600000&Build=19471' 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=indihiang&DownloadId=112614&FileTime=129139805751000000&Build=19471'
