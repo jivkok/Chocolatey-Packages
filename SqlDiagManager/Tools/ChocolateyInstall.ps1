81103c5a60915f74549c2dd255b19cc4c7098bcf
Jivko Kolev <jivkokgit@gmail.com>
2012-10-08 21:01:37 -0700

    Adding SqlDiagManager

---
+++ SqlDiagManager/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,13 @@
+$package = 'sqldiagmanager';
+
+try {
+    $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    $msi = "$drop\SetupX86.msi"
+    Install-ChocolateyZipPackage $package 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=diagmanager&DownloadId=246133&FileTime=129514200362570000&Build=19471' $drop
+    Install-ChocolateyInstallPackage $package 'msi' "/passive" $msi
+
+    Write-ChocolateySuccess $package
+} catch {
+    Write-ChocolateyFailure $package $($_.Exception.Message)
+    throw 
+}
