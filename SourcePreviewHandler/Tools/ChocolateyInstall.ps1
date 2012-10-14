1acdf4b229672bb98e202abe178f5434dfc4ae97
Jivko Kolev <jivkokgit@gmail.com>
2012-10-13 17:00:08 -0700

    Adding SourcePreviewHandler

---
+++ SourcePreviewHandler/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,3 @@
+$package = 'SourcePreviewHandler';
+
+Install-ChocolateyPackage $package 'msi' '/passive' 'http://www.smartftp.com/get/SourcePreviewHandler.msi' 'http://www.smartftp.com/get/SourcePreviewHandler64.msi'
