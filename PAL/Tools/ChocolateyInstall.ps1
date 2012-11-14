86ba3eb8eae5f0fbcee98dd37048b0dd87b62498
Jivko Kolev <jivkokgit@gmail.com>
2012-11-14 13:06:45 -0800

    Updating PAL

--- PAL/Tools/ChocolateyInstall.ps1
+++ PAL/Tools/ChocolateyInstall.ps1
@@ -3,7 +3,7 @@ $package = 'pal';
 try {
     $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
     $msi = "$drop\PAL_Setup.msi"
-    Install-ChocolateyZipPackage $package 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pal&DownloadId=453991&FileTime=129870355259100000&Build=19471' $drop
+    Install-ChocolateyZipPackage $package 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pal&DownloadId=453991&FileTime=129870355259100000&Build=74' $drop
     Install-ChocolateyInstallPackage $package 'msi' "/passive" $msi
 
     Write-ChocolateySuccess $package
