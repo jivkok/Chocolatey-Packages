1c0759ab35aeab2efef5bd0ee970715af9128e18
Jivko Kolev <jivkokgit@gmail.com>
2012-11-13 23:05:31 -0800

    Adding WindowsPhone8SDK

---
+++ WindowsPhone8SDK/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyPackage 'WindowsPhone8SDK' 'exe' "/Passive /NoRestart /Log $env:temp\WP8.log" 'http://download.microsoft.com/download/9/3/8/938A5074-461F-4E3D-89F4-5CE2F42C1E36/fulltril30/exe/WPexpress_full.exe'
