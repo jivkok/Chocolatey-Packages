8f2ac2aa79cd8524d9e1db8a4578a4065100e93f
Jivko Kolev <jivkokgit@gmail.com>
2013-05-19 11:13:15 -0700

    Updating SourceTree

--- SourceTree/Tools/ChocolateyInstall.ps1
+++ SourceTree/Tools/ChocolateyInstall.ps1
@@ -1,18 +1,1 @@
-$package = 'SourceTree';
-
-try {
-    $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition);
-    $installerAuto = Join-Path $scriptDir 'SourceTree.au3';
-
-    $installerPackage = Join-Path $scriptDir "SourceTreeSetup.exe";
-    Get-ChocolateyWebFile $package $installerPackage 'http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup.exe';
-  
-    Write-Host "Installing `'$installerPackage`' with AutoIt3 using `'$installerAuto`'"
-    $installArgs = "/c autoit3 `"$installerAuto`" `"$installerPackage`""
-    Start-ChocolateyProcessAsAdmin "$installArgs" "cmd.exe"
-
-    Write-ChocolateySuccess $package
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw 
-}
+Install-ChocolateyPackage 'SourceTree' 'exe' "/passive" 'http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_0.9.2.3.exe'
