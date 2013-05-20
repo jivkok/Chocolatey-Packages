6cc5ae4b4133ade7188474ee8cf885273bab8f5e
Jivko Kolev <jivkokgit@gmail.com>
2013-05-19 20:41:55 -0700

    Updating MySql.Workbench

--- MySql.Workbench/Tools/ChocolateyInstall.ps1
+++ MySql.Workbench/Tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'mysql.workbench' 'msi' '/passive' 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-gpl-5.2.44-win32.msi'
+Install-ChocolateyPackage 'mysql.workbench' 'msi' '/passive' 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-gpl-5.2.47-win32.msi'
