0b9c9e9f2f824bda6bc2310d75e82d172daf3fad
Jivko Kolev <jivkokgit@gmail.com>
2013-07-10 01:37:49 -0700

    Updating SourceTree

--- SourceTree/Tools/ChocolateyInstall.ps1
+++ SourceTree/Tools/ChocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'SourceTree' 'exe' "/passive" 'http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_0.9.4.exe'
+Install-ChocolateyPackage 'SourceTree' 'exe' "/passive" 'http://downloads.atlassian.com/software/sourcetree/windows/SourceTreeSetup_1.0.5.exe'
