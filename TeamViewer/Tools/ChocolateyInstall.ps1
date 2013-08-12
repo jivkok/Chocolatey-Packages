7833884a7444a0a5a3d62e196a1ed1190facf32c
Antonio David Gellida Lavara <venerable13@gmail.com>
2013-08-12 12:40:18 +0200

    Converting to automatic

--- TeamViewer/Tools/ChocolateyInstall.ps1
+++ TeamViewer/Tools/ChocolateyInstall.ps1
@@ -1,6 +1,7 @@
-﻿$packageName = 'teamviewer'
+$packageName = '{{PackageName}}'
 $fileType = 'exe'
 $silentArgs = '/S'
-$url = 'http://download.teamviewer.com/download/TeamViewer_Setup.exe'
+#$url = 'http://download.teamviewer.com/download/TeamViewer_Setup.exe'
+$url = '{{DownloadUrl}}'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
