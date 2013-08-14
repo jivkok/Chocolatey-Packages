07bf5670f25c741c9fc56e4f173639a7b5bca50b
TomOne <user.email@fake.com>
2013-08-14 02:59:01 +0200

    restoring accidentialy modified file

--- XMind/Tools/ChocolateyUninstall.ps1
+++ XMind/Tools/ChocolateyUninstall.ps1
@@ -1,1 +1,15 @@
-﻿
+﻿try {
+  $processor = Get-WmiObject Win32_Processor
+  $is64bit = $processor.AddressWidth -eq 64
+  if ($is64bit) {
+    $unpath = "${Env:ProgramFiles(x86)}\XMind\uninstall.exe"
+  } else {
+    $unpath = "${Env:ProgramFiles}\XMind\uninstall.exe"
+  }
+  Uninstall-ChocolateyPackage 'xmind' 'EXE' '/S' "$unpath" -validExitCodes @(0)
+  
+  Write-ChocolateySuccess 'xmind'
+} catch {
+  Write-ChocolateyFailure 'xmind' "$($_.Exception.Message)"
+  throw 
+}
