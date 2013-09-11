27742fe39533ef258370c9eb64062be000e9240f
Jivko Kolev <jivkokgit@gmail.com>
2013-09-11 08:06:25 -0700

    Updating ResophNotes

--- ResophNotes/Tools/ChocolateyInstall.ps1
+++ ResophNotes/Tools/ChocolateyInstall.ps1
@@ -3,11 +3,11 @@ $package = 'ResophNotes';
 try {
     $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
     $msi = "$drop\ResophNotes.msi"
-    Install-ChocolateyZipPackage $package 'http://resoph.com/ResophNotes/Welcome_files/ResophNotes153.zip' $drop
+    Install-ChocolateyZipPackage $package 'http://resoph.com/ResophNotes/Welcome_files/ResophNotes155.zip' $drop
     Install-ChocolateyInstallPackage $package 'msi' "/passive" $msi
 
     Write-ChocolateySuccess $package
 } catch {
     Write-ChocolateyFailure $package $($_.Exception.Message)
-    throw 
+    throw
 }
