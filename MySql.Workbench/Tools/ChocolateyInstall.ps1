a0066aee60f46cc5097d853edf8f57c708fa5c7c
TomOne <user.email@fake.com>
2013-09-16 22:08:09 +0200

    removed deprecated packages that have been moved to automatic packages

--- MySql.Workbench/Tools/ChocolateyInstall.ps1
+++
@@ -1,1 +0,0 @@
-﻿Install-ChocolateyPackage 'mysql.workbench' 'msi' '/passive' 'http://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-gpl-5.2.47-win32.msi'
--- TeamViewer/Tools/ChocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$packageName = 'teamviewer'
-$fileType = 'exe'
-$silentArgs = '/S'
-$url = 'http://download.teamviewer.com/download/TeamViewer_Setup.exe'
-
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
