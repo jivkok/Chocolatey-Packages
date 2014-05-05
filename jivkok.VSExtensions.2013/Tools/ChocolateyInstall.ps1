1d0204ccbb3879f3cf1cee38da2e477ac20f7017
Jivko Kolev <jivkokgit@gmail.com>
2014-05-05 00:20:26 -0700

    Updating jivkok.VSExtensions.2013

--- jivkok.VSExtensions.2013/Tools/ChocolateyInstall.ps1
+++ jivkok.VSExtensions.2013/Tools/ChocolateyInstall.ps1
@@ -44,12 +44,16 @@ try {
     installsilently http://visualstudiogallery.msdn.microsoft.com/4ec1526c-4a8c-4a84-b702-b21a8f5293ca/file/105933/4/NuGet.Tools.2013.vsix NuGet.vsix
     installsilently http://visualstudiogallery.msdn.microsoft.com/ad0897b3-7537-4c92-a38c-104b0e005206/file/75983/4/PerfWatsonMonitor.vsix PerfWatsonMonitor.vsix
     installsilently http://visualstudiogallery.msdn.microsoft.com/a058d5d3-e654-43f8-a308-c3bdfdd0be4a/file/89212/35/PostSharp-3.1.27.vsix PostSharp.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/c9eb3ba8-0c59-4944-9a62-6eee37294597/file/112013/7/PowerShellTools.vsix PowerShellTools.vsix
     installsilently http://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/1/ProPowerTools.vsix ProPowerTools.vsix
     installsilently http://visualstudiogallery.msdn.microsoft.com/f0589156-a8e6-47db-8bac-90f01ca6b8a3/file/80662/4/Cobisi.RoutingAssistant-v1.8.vsix RoutingAssistant.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/b08b0375-139e-41d7-af9b-faee50f68392/file/5131/8/SnippetDesigner.vsix SnippetDesigner.vsix
     installsilently http://visualstudiogallery.msdn.microsoft.com/90ac3587-7466-4155-b591-2cd4cc4401bc/file/112721/3/TechTalk.SpecFlow.Vs2013Integration.vsix SpecFlow.vsix
     installsilently http://visualstudiogallery.msdn.microsoft.com/f4d9c2b5-d6d7-4543-a7a5-2d7ebabc2496/file/63103/7/VSColorOutput.vsix VSColorOutput.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/c6d1c265-7007-405c-a68b-5606af238ece/file/106247/16/SquaredInfinity.VSCommands.VS12.vsix VSCommands.vsix
     installsilently http://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/45/VsVim.vsix VsVim.vsix
     installsilently http://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/23/WebEssentials2013.vsix WebEssentials.vsix
+    installsilently http://visualstudiogallery.msdn.microsoft.com/2b96d16a-c986-4501-8f97-8008f9db141a/file/53962/59/Mindscape.WebWorkbench.Integration.10.vsix WebWorkbench.vsix
 
     Write-ChocolateySuccess $packageName
 } catch {
