16af1b63cca636ae581de8247d6f4e2b3ad3091b
Jivko Kolev <jivkokgit@gmail.com>
2014-11-25 19:06:44 -0800

    Viewers for Word, Excel, and Powerpoint

---
+++ Excel.Viewer/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyPackage 'Excel.Viewer' 'exe' '/quiet' 'http://download.microsoft.com/download/e/a/9/ea913c8b-51a7-41b7-8697-9f0d0a7274aa/ExcelViewer.exe'
---
+++ Word.Viewer/Tools/ChocolateyInstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyPackage 'Word.Viewer' 'exe' '/quiet' 'http://download.microsoft.com/download/6/a/6/6a689355-b155-4fa7-ad8a-dfe150fe7ac6/wordview_en-us.exe'
