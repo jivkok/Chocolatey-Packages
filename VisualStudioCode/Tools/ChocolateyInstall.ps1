5a3a4c698feb685c11110a87045d43812a735759
Jivko Kolev <jivkokgit@gmail.com>
2015-05-02 16:52:16 -0700

    VisualStudioCode fixes

--- VisualStudioCode/Tools/ChocolateyInstall.ps1
+++ VisualStudioCode/Tools/ChocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'VisualStudioCode'
 $installerType = 'exe'
-$silentArgs = '' #TODO
+$silentArgs = '-s'
 $32BitUrl  = 'http://download.microsoft.com/download/0/D/5/0D57186C-834B-463A-AECB-BC55A8E466AE/VSCodeSetup.exe'
 $validExitCodes = @(
     0 # success
