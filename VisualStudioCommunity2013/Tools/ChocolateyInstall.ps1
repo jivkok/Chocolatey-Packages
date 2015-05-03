9201523f45238e1b0a2483cedd3ce31f3a1b3f7a
Jivko Kolev <jivkokgit@gmail.com>
2015-05-03 15:05:29 -0700

    Use common VS module for shared functionality

--- VisualStudioCommunity2013/Tools/ChocolateyInstall.ps1
+++ VisualStudioCommunity2013/Tools/ChocolateyInstall.ps1
@@ -1,18 +1,3 @@
-. (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'functions.ps1')
+Import-Module (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'VSModules.psm1')
 
-$packageParameters = Parse-Parameters $env:chocolateyPackageParameters
-Write-Output $packageParameters
-$adminFile = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'AdminDeployment.xml')
-Update-Admin-File $packageParameters $adminFile
-$silentArgs = Generate-Install-Arguments-String $packageParameters $adminFile
-
-$packageName = 'VisualStudioCommunity2013'
-$installerType = 'exe'
-$32BitUrl  = 'http://download.microsoft.com/download/7/1/B/71BA74D8-B9A0-4E6C-9159-A8335D54437E/vs_community.exe'
-$validExitCodes = @(
-    0, # success
-    3010 # success, restart required
-)
-
-Write-Output "Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl -validExitCodes $validExitCodes"
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
+Install-VS 'VisualStudioCommunity2013' 'http://download.microsoft.com/download/7/1/B/71BA74D8-B9A0-4E6C-9159-A8335D54437E/vs_community.exe'
--- VisualStudioCommunity2013/Tools/ChocolateyUninstall.ps1
+++ VisualStudioCommunity2013/Tools/ChocolateyUninstall.ps1
@@ -1,14 +1,3 @@
-$packageName = 'VisualStudioCommunity2013'
-$installerType = 'exe'
-$silentArgs = '/Uninstall /force /Passive /NoRestart'
-$appName = 'Microsoft Visual Studio Community 2013'
+Import-Module (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'VSModules.psm1')
 
-$app = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq $appName }
-if ($app -ne $null)
-{
-    $uninstaller = Get-Childitem "$env:ProgramData\Package Cache\" -Recurse -Filter vs_community.exe | ? { $_.VersionInfo.ProductVersion.StartsWith($app.Version)}
-    if ($uninstaller -ne $null)
-    {
-        Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" $uninstaller.FullName
-    }
-}
+Uninstall-VS 'VisualStudioCommunity2013' 'Microsoft Visual Studio Community 2013' 'vs_community.exe'
