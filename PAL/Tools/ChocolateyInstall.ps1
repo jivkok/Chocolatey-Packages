0972b7d9af86fc51ef9f34cf6eb7db5ca2a6cf3d
David Gardiner <david@gardiner.net.au>
2015-07-06 14:38:04 +0930

    Update link to PAL 2.7.4

--- PAL/Tools/ChocolateyInstall.ps1
+++ PAL/Tools/ChocolateyInstall.ps1
@@ -3,7 +3,7 @@ $package = 'pal';
 try {
     $drop = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
     $msi = "$drop\PAL_Setup.msi"
-    Install-ChocolateyZipPackage $package 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pal&DownloadId=453991&FileTime=129870355259100000&Build=74' $drop
+    Install-ChocolateyZipPackage $package 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pal&DownloadId=1458063&FileTime=130766179509630000&Build=21024' $drop
     Install-ChocolateyInstallPackage $package 'msi' "/passive" $msi
 } catch {
     Write-ChocolateyFailure $package $($_.Exception.Message)
