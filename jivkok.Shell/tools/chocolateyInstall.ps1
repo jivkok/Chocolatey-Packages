a2f095492ca26d1ec6fa475e05843d27f4665adb
Jivko Kolev <jivkokgit@gmail.com>
2015-08-02 20:18:38 -0700

    Redo jivkok.shell - use the dotfiles repo

--- jivkok.Shell/tools/chocolateyInstall.ps1
+++ jivkok.Shell/tools/chocolateyInstall.ps1
@@ -1,65 +1,21 @@
 $package = 'jivkok.Shell'
 
-function Get-CurrentDirectory
-{
-  $thisName = $MyInvocation.MyCommand.Name
-  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
-}
-
-function New-Shortcut($ShortcutPath, $TargetPath, $Arguments, $IconLocation)
-{
-    $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut($ShortcutPath)
-    $Shortcut.TargetPath = $TargetPath
-    $Shortcut.Arguments = $Arguments
-    $Shortcut.IconLocation = $IconLocation
-    $Shortcut.Save()
-}
-
 try {
-  $currentPath = Get-CurrentDirectory
-
-  $destPath = "$Home\.shell"
-  if (!(Test-Path $destPath))
-  {
-    New-Item $destPath -Type Directory | Out-Null
+  # dotfiles repo
+  if (Test-Path "$Home\dotfiles\.git") {
+    . "${Env:ProgramFiles(x86)}\Git\bin\git.exe" -C "$Home\dotfiles" pull --prune --recurse-submodules
+    . "${Env:ProgramFiles(x86)}\Git\bin\git.exe" -C "$Home\dotfiles" submodule init
+    . "${Env:ProgramFiles(x86)}\Git\bin\git.exe" -C "$Home\dotfiles" submodule update --remote --recursive
+  } else {
+    if (Test-Path "$Home\dotfiles") {
+      if (Test-Path "$Home\dotfiles.BACKUP") { Remove-Item "$Home\dotfiles.BACKUP" -Recurse -Force }
+      Rename-Item "$Home\dotfiles" "$Home\dotfiles.BACKUP" -Force
+    }
+    . "${Env:ProgramFiles(x86)}\Git\bin\git.exe" clone --recursive https://github.com/jivkok/dotfiles.git "$Home\dotfiles"
   }
 
-  # Copy files
-  'Console.xml',
-  'SetEnv.cmd',
-  'SetPosh.ps1' |
-    % {
-      $params = @{
-        Path = Join-Path $currentPath $_;
-        Destination = Join-Path $destPath $_;
-        Force = $true
-      }
-      Copy-Item @params
-    }
-
-  New-Shortcut "$Home\Desktop\Console.lnk" "$Env:ChocolateyInstall\bin\Console.exe" "-c $Home\.shell\console.xml" "cmd.exe,0"
-
-  # Bash files
-  $destPath = "$Home"
-  copy (Join-Path $currentPath Bash.aliases) (Join-Path $destPath .aliases)
-  copy (Join-Path $currentPath Bash.bashrc) (Join-Path $destPath .bashrc)
-  copy (Join-Path $currentPath Bash.bash_profile) (Join-Path $destPath .bash_profile)
-  copy (Join-Path $currentPath Bash.bash_prompt) (Join-Path $destPath .bash_prompt)
-  copy (Join-Path $currentPath Bash.exports) (Join-Path $destPath .exports)
-  copy (Join-Path $currentPath Bash.functions) (Join-Path $destPath .functions)
-
-  # BoxStarter
-  Import-Module (Join-Path $currentPath BoxStarter.psm1)
-  Disable-ShutdownTracker
-  Disable-InternetExplorerESC
-  Set-ExplorerOptions -showHidenFilesFoldersDrives $true -showProtectedOSFiles $true -showFileExtensions $true
-  Enable-RemoteDesktop
-  # Set-TaskbarSmall
-  # Restart-Explorer
-
-  [Environment]::SetEnvironmentVariable("_NT_SYMBOL_PATH", "srv*C:\Symbols*http://referencesource.microsoft.com/symbols*http://srv.symbolsource.org/pdb/Public*http://msdl.microsoft.com/download/symbols", "User")
+  . "$Home\dotfiles\setup_windows.ps1"
 } catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  Write-ChocolateyFailure $package "$_"
   throw
 }
--- jivkok.Shell/tools/chocolateyUninstall.ps1
+++ jivkok.Shell/tools/chocolateyUninstall.ps1
@@ -1,9 +1,40 @@
 $package = 'jivkok.Shell'
 
 try {
-    Remove-Item "$Home\.shell" -recurse -force
-    Remove-Item "$Home\Desktop\JConsole.lnk" -force
+  echo "Deleting desktop shortcut (and taskbar pin): $Home\Desktop\Shell.lnk"
+  if (Test-Path "$Home\Desktop\Shell.lnk") {
+    $shellApp = New-Object -c Shell.Application
+    $folderItem = $shellApp.Namespace("$Home\Desktop").ParseName('Shell.lnk')
+    $folderItem.InvokeVerb('taskbarunpin')
+
+    Remove-Item "$Home\Desktop\Shell.lnk" -Force
+  }
+
+  echo 'Deleting directory symlinks'
+  '.vim' |
+    % {
+      if (Test-Path "$Home\$_.BACKUP") { cmd /c "rd $Home\$_.BACKUP" }
+      Rename-Item "$Home\$_" "$Home\$_.BACKUP" -Force
+    }
+
+  echo 'Deleting file symlinks'
+  '.aliases',
+  '.bash_profile',
+  '.bash_prompt',
+  '.bashrc',
+  '.curlrc',
+  '.exports',
+  '.functions',
+  '.tmux.conf',
+  '.vimrc',
+  '.wgetrc' |
+    % {
+      if (Test-Path "$Home\$_.BACKUP") { Remove-Item "$Home\$_.BACKUP" -Force }
+      Rename-Item "$Home\$_" "$Home\$_.BACKUP" -Force
+    }
+
+  echo 'NOTE: repo is left in-place'
 } catch {
-    Write-ChocolateyFailure $package "$($_.Exception.Message)"
-    throw
+  Write-ChocolateyFailure $package "$_"
+  throw
 }
