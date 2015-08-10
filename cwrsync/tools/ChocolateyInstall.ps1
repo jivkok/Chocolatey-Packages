b062ee0362a2461c949435b94c53de3e3778681d
Jivko Kolev <jivkokgit@gmail.com>
2015-08-09 19:45:54 -0700

    Own version of the cwrsync package

---
+++ cwrsync/tools/ChocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = 'cwrsync'
+$url = 'https://www.itefix.net/dl/cwRsync_5.4.1_x86_Free.zip'
+$version = '5.4.1'
+
+try {
+  $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  $packageFilesDir = Join-Path $toolsDir "cwRsync_${version}_x86_Free"
+
+  Install-ChocolateyZipPackage $packageName $url $toolsDir
+
+  Move-Item "$packageFilesDir\*.*" $toolsDir
+
+  Remove-Item $packageFilesDir
+} catch {
+  Write-ChocolateyFailure $packageName "$_"
+  throw
+}
