e3a42f739c95ece681166e4c979439d795630250
Jivko Kolev <jivkokgit@gmail.com>
2015-08-17 00:22:40 -0700

    DotNet4.6

--- DotNet4.6/Tools/ChocolateyInstall.ps1
+++ DotNet4.6/Tools/ChocolateyInstall.ps1
@@ -1,9 +1,13 @@
-$test = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -eq 'Microsoft .NET Framework 4.6 RC Multi-Targeting Pack' } | Select-Object -First 1
-if (!$test)
+$test = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full').Release
+if (!($test -eq $null) -and ($test -eq 393295 -or $test -eq 393297))
+{
+    Write-Host "Microsoft .Net 4.6 Framework is already installed on your machine."
+}
+else
 {
     $packageName = 'DotNet46'
     $installerType = 'exe'
-    $32BitUrl  = 'http://download.microsoft.com/download/9/E/E/9EEF3485-8C04-47D0-93AF-7E0591AFED10/NDP46-KB3033092-Web.exe'
+    $32BitUrl  = 'http://download.microsoft.com/download/1/4/A/14A6C422-0D3C-4811-A31F-5EF91A83C368/NDP46-KB3045560-Web.exe'
     $silentArgs = "/Passive /NoRestart /Log $env:temp\net46.log"
     $validExitCodes = @(
         0, # success
@@ -11,8 +15,4 @@ if (!$test)
     )
 
     Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl -validExitCodes $validExitCodes
-}
-else
-{
-    Write-Host "Microsoft .Net 4.6 Framework is already installed on your machine."
 }
