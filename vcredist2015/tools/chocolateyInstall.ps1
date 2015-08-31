63be5e1137a04b91fed3328ac10b4417b86abb21
Jivko Kolev <jivkokgit@gmail.com>
2015-08-31 03:43:52 -0700

    vcredist2015

---
+++ vcredist2015/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'vcredist2015'
+$installerType = 'exe'
+$url = 'http://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x86.exe'
+$url64 = 'http://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe'
+$silentArgs = '/Q'
+$validExitCodes = @(
+    0, # success
+    3010, # success, restart required
+    2147781575, # pending restart required
+    -2147185721 # pending restart required
+)
+
+Install-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -Url "$url" -Url64bit "$url64" -SilentArgs "$silentArgs" -ValidExitCodes $validExitCodes
+
+if (Get-ProcessorBits 64) {
+    Install-ChocolateyPackage -PackageName "$packageName" -FileType "$installerType" -Url "$url" -SilentArgs "$silentArgs" -ValidExitCodes $validExitCodes
+}
---
+++ vcredist2015/tools/chocolateyUninstall.ps1
@@ -0,0 +1,28 @@
+﻿$packageName = 'vcredist2015'
+$packageSearch = 'Microsoft Visual C++ 2015 Redistributable'
+$installerType = 'exe'
+$silentArgs = '/uninstall /quiet'
+$validExitCodes = @(
+    0, # success
+    3010, # success, restart required
+    2147781575, # pending restart required
+    -2147185721 # pending restart required
+)
+
+try {
+  Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                            'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                   -ErrorAction:SilentlyContinue `
+  | Where-Object { $_.DisplayName -Like "$packageSearch*" } `
+  | Select-Object BundleCachePath -Unique `
+  | ForEach-Object {
+    $file = $_.BundleCachePath
+    Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                -FileType "$installerType" `
+                                -SilentArgs "$silentArgs" `
+                                -File "$file" `
+                                -ValidExitCodes $validExitCodes
+  }
+} catch {
+  throw $_.Exception
+}
