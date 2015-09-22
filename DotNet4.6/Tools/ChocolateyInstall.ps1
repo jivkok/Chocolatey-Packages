e0238323897a3a076a8b5d67b22ad261c0865ce4
Ash Caire <ash.caire@gmail.com>
2015-09-23 08:41:49 +1000

    Implement DotNet4.6 package changes as recommended by @jberezanski

--- DotNet4.6/Tools/ChocolateyInstall.ps1
+++ DotNet4.6/Tools/ChocolateyInstall.ps1
@@ -1,18 +1,24 @@
-$test = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full').Release
-if (!($test -eq $null) -and ($test -eq 393295 -or $test -eq 393297))
-{
-    Write-Host "Microsoft .Net 4.6 Framework is already installed on your machine."
+Function IsInstalled {
+    $ver = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full').Release
+    return (!($ver -eq $null) -and ($ver -ge 393295))
 }
-else
-{
+
+if (IsInstalled) {
+    Write-Host "Microsoft .NET Framework 4.6 or later is already installed"
+}
+else {
     $packageName = 'DotNet46'
     $installerType = 'exe'
-    $32BitUrl  = 'http://download.microsoft.com/download/1/4/A/14A6C422-0D3C-4811-A31F-5EF91A83C368/NDP46-KB3045560-Web.exe'
-    $silentArgs = "/Passive /NoRestart /Log $env:temp\net46.log"
+    $Url = 'http://download.microsoft.com/download/C/3/A/C3A5200B-D33C-47E9-9D70-2F7C65DAAD94/NDP46-KB3045557-x86-x64-AllOS-ENU.exe'
+    $silentArgs = "/Passive /NoRestart /Log ""$env:temp\net46.log"""
     $validExitCodes = @(
         0, # success
         3010 # success, restart required
     )
 
-    Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl -validExitCodes $validExitCodes
+    Install-ChocolateyPackage $packageName $installerType $silentArgs $Url -validExitCodes $validExitCodes
+
+    if (-Not (IsInstalled)) {
+        Write-Host "A restart is required to finalise the Microsoft .NET Framework 4.6 installation"
+    }
 }
