662214ada99bbeaf578f612f6fd781271ef4272d
Jivko Kolev <jivkokgit@gmail.com>
2015-09-26 16:25:00 -0700

    DotNet4.6 updates

--- DotNet4.6-TargetPack/Tools/ChocolateyInstall.ps1
+++ DotNet4.6-TargetPack/Tools/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageName = 'DotNet46-TargetPack'
 $installerType = 'exe'
 $32BitUrl  = 'http://download.microsoft.com/download/8/2/F/82FF2034-83E6-4F93-900D-F88C7AD9F3EE/NDP46-TargetingPack-KB3045566.exe'
-$silentArgs = "/Passive /NoRestart /Log $env:temp\net46-targetpack.log"
+$silentArgs = "/Passive /NoRestart /Log ""$env:temp\net46-targetpack.log"""
 $validExitCodes = @(
     0, # success
     3010 # success, restart required
@@ -12,6 +12,6 @@ Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl -val
 # ENU language pack
 $packageName = 'DotNet46-TargetPack-enu'
 $32BitUrl  = 'http://download.microsoft.com/download/8/2/F/82FF2034-83E6-4F93-900D-F88C7AD9F3EE/NDP46-TargetingPack-KB3045566-ENU.exe'
-$silentArgs = "/Passive /NoRestart /Log $env:temp\net46-targetpackenu.log"
+$silentArgs = "/Passive /NoRestart /Log ""$env:temp\net46-targetpackenu.log"""
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $32BitUrl -validExitCodes $validExitCodes
--- DotNet4.6/Tools/ChocolateyInstall.ps1
+++ DotNet4.6/Tools/ChocolateyInstall.ps1
@@ -1,14 +1,14 @@
 $test = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full').Release
-if (!($test -eq $null) -and ($test -eq 393295 -or $test -eq 393297))
+if (!($test -eq $null) -and ($test -ge 393295))
 {
-    Write-Host "Microsoft .Net 4.6 Framework is already installed on your machine."
+    Write-Host "Microsoft .NET Framework 4.6 is already installed on your machine."
 }
 else
 {
     $packageName = 'DotNet46'
     $installerType = 'exe'
     $32BitUrl  = 'http://download.microsoft.com/download/1/4/A/14A6C422-0D3C-4811-A31F-5EF91A83C368/NDP46-KB3045560-Web.exe'
-    $silentArgs = "/Passive /NoRestart /Log $env:temp\net46.log"
+    $silentArgs = "/Passive /NoRestart /Log ""$env:temp\net46.log"""
     $validExitCodes = @(
         0, # success
         3010 # success, restart required
