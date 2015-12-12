c060951b5dbfe87812d854eb9a91af4573f639b6
Jivko Kolev <jivkokgit@gmail.com>
2015-12-12 11:39:06 -0800

    VSCode moved to automatic packages

--- VisualStudioCode/Tools/ChocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-$packageName = 'VisualStudioCode'
-$installerType = 'exe'
-$silentArgs = "/silent /log=""$env:temp\vscode.log"""
-$32BitUrl  = 'https://az764295.vo.msecnd.net/public/0.8.0/VSCodeSetup.exe'
-$validExitCodes = @(
-    0 # success
-)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
