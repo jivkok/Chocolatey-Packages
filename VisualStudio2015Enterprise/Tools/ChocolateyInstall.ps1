69ed4c9d0782e2ca3d61c786016c2654f7872678
Rami Abughazaleh <icnocop@noreply.users.github.com>
2016-01-27 01:17:56 -0800

    Fixed https://github.com/jivkok/Chocolatey-Packages/issues/25: Updated to Visual Studio 2015 with Update 1

--- VisualStudio2015Enterprise/Tools/ChocolateyInstall.ps1
+++ VisualStudio2015Enterprise/Tools/ChocolateyInstall.ps1
@@ -1,3 +1,3 @@
 Import-Module (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'VSModules.psm1')
 
-Install-VS 'VisualStudio2015Enterprise' 'http://download.microsoft.com/download/6/4/7/647EC5B1-68BE-445E-B137-916A0AE51304/vs_enterprise.exe'
+Install-VS 'VisualStudio2015Enterprise' 'http://download.microsoft.com/download/C/7/8/C789377D-7D49-4331-8728-6CED518956A0/vs_enterprise_ENU.exe'
