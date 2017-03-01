179d6d93545708045ad3d8e48d593184da989552
Jakub Bereżański <kuba@berezanscy.pl>
2017-03-01 23:02:11 +0100

    VisualStudio2017*: uninstall only the appropriate product, not all VS 2017 products

--- VisualStudio2017Community/Tools/ChocolateyUninstall.ps1
+++ VisualStudio2017Community/Tools/ChocolateyUninstall.ps1
@@ -1,6 +1,4 @@
-Uninstall-VisualStudio `
-    -PackageName 'VisualStudio2017Community' `
-    -ApplicationName 'Microsoft Visual Studio Community 2017 RC' `
-    -UninstallerName 'vs_installer.exe' `
-    -InstallerTechnology 'WillowVS2017OrLater' `
-    -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
+Remove-VisualStudioProduct `
+    -PackageName 'VisualStudio2017Community' `
+    -Product 'Community' `
+    -VisualStudioYear '2017'
--- VisualStudio2017Professional/Tools/ChocolateyUninstall.ps1
+++ VisualStudio2017Professional/Tools/ChocolateyUninstall.ps1
@@ -1,6 +1,4 @@
-Uninstall-VisualStudio `
-    -PackageName 'VisualStudio2017Professional' `
-    -ApplicationName 'Microsoft Visual Studio Professional 2017 RC' `
-    -UninstallerName 'vs_installer.exe' `
-    -InstallerTechnology 'WillowVS2017OrLater' `
-    -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
+Remove-VisualStudioProduct `
+    -PackageName 'VisualStudio2017Professional' `
+    -Product 'Professional' `
+    -VisualStudioYear '2017'
