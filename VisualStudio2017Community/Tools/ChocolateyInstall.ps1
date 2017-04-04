66036c5c3e4e0e55fd34a99b65738558119cb435
Jakub Bereżański <kuba@berezanscy.pl>
2017-04-04 21:55:45 +0200

    update visualstudio2017 products to version 26228.12 (2017-03-31)

--- VisualStudio2017Community/Tools/ChocolateyInstall.ps1
+++ VisualStudio2017Community/Tools/ChocolateyInstall.ps1
@@ -1,8 +1,8 @@
 Install-VisualStudio `
     -PackageName 'VisualStudio2017Community' `
     -ApplicationName 'Microsoft Visual Studio Community 2017' `
-    -Url 'https://download.microsoft.com/download/9/D/2/9D228A37-56C0-48D7-B1B4-486090DE7C2A/vs_Community.exe' `
-    -Checksum '2C09F31BF870D9073FD707EA4CB1700E7191C4D9E5D5A3AD9F43F0DAFFF2E856' `
+    -Url 'https://download.microsoft.com/download/5/2/1/521C52A7-EFCC-46CE-A890-4F317732ABA3/vs_Community.exe' `
+    -Checksum 'A964FA078C0FA9115001302FF80D8119995814B58322646A46C4F3B894479471' `
     -ChecksumType 'SHA256' `
     -InstallerTechnology 'WillowVS2017OrLater' `
     -ProgramsAndFeaturesDisplayName 'Microsoft Visual Studio 2017'
